b0184651091f20864367a72c23e1044e1bb48611
Julian Ehiem <jules.ehiem@gmail.com>
2020-11-02 06:46:54 -0500

    changed temperature units

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
 
     return (
         <div className="main-container">
-            <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
+            <input type="text"className="search"placeholder="Enter City"value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
             {weather.main && (
                 <div className="city">
                     <h2 className="city-name">
@@ -27,7 +27,7 @@ const App = () => {
                     </h2>
                     <div className="city-temp">
                         {Math.round(weather.main.temp)}
-                        <sup>&deg;C</sup>
+                        <sup>&deg;F</sup>
                     </div>
                     <div className="info">
                         <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
