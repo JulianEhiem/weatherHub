9f6b47dc217118df605ed8447d7c9d987ffb92e2
Julian Ehiem <jules.ehiem@gmail.com>
2021-03-26 19:21:04 -0400

    added new search components

--- src/App.js
+++ src/App.js
@@ -40,15 +40,23 @@ const App = () => {
                                         </h2>
                                     </div>  
                                 </div>
-                                <div className="searchArea">
-                                    <div className="logo-container">
-                                        <img src="./images/wh-logo.png" alt="Weather Hub Logo" className="logo-img"></img>
-                                    </div>
-                                    <div className="search-container">
-                                     <input type="text"className="search"placeholder="Enter City"value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
-                                     <button className="search-btn">
+                                <div className="secondarySearchArea">
+                                    <div className="newSearch">
+                                        <input type="text"className="searchNew"placeholder="Enter City"value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
+                                        <button className="search-btn">
                                          <img src={searchIcon} alt="Search Icon"/>
-                                     </button>
+                                        </button>
+                                    </div>
+                                    <div className="weather-details">
+                                        <h3 className="section-subtitle">Weather Details</h3>
+                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam enim modi eos nemo officiis! Ducimus quisquam ex, amet tenetur adipisci cum explicabo dignissimos est, provident reiciendis, ullam voluptates sequi dolore!
+
+                                     </p>
+                                    </div>
+                                    <div className="cloud-cover">
+                                        <h3 className="data-title">Cloud Cover</h3>
+                                        <h3 className="data-result">88%</h3>
+
                                     </div>
                                 </div>
                             </div>
--- src/components/DisplayArea.js
+++ src/components/DisplayArea.js
@@ -1,15 +1,59 @@
+import React, { useState } from 'react';
+import { fetchWeather } from '../api/fetchWeather';
+import searchIcon from '../icons/searchIcon.svg';
+
 
 
 export const DisplayArea = () => {
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+    
+    const search = async (e) => {
+        if(e.key === 'Enter') {
+            const data = await fetchWeather(query);
+
+            setWeather(data);
+            setQuery('');
+        }
+    }
+
     return (
-        <div className="temp-city-group">
-            <div className="city-temp">
-                {Math.round(weather.main.temp)}
-                <sup>&deg;</sup>F
-            </div>
-            <h2 className="city-name">
-                {weather.name}
-            </h2>
-        </div>
+        <>
+                <div className="appContainer">
+                    <div className="blur-container"></div>
+                    <div className="mainContainer">
+                        <div className="theSection">
+                            <div className="mainSection">
+                                <div className="displayArea">
+                                    <div className="logo-container-2">
+                                        <img src="./images/wh-logo.png" alt="Weather Hub Logo" className="logo-img-2"></img>
+                                    </div>
+                                    
+                                    <div className="temp-city-group">
+                                        <div className="city-temp">
+                                            {Math.round(weather.main.temp)}
+                                            <sup>&deg;</sup>F
+                                        </div>
+                                        <h2 className="city-name">
+                                            {weather.name}
+                                        </h2>
+                                    </div>  
+                                </div>
+                                <div className="searchArea">
+                                    <div className="logo-container">
+                                        <img src="./images/wh-logo.png" alt="Weather Hub Logo" className="logo-img"></img>
+                                    </div>
+                                    <div className="search-container">
+                                     <input type="text"className="search"placeholder="Enter City"value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
+                                     <button className="search-btn">
+                                         <img src={searchIcon} alt="Search Icon"/>
+                                     </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
     );
 }
