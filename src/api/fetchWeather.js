43275a0f2849e5caa4e7da76fb4311b8204366bb
Julian Ehiem <jules.ehiem@gmail.com>
2021-03-28 22:08:10 -0400

    updated axios and gitignore

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,12 +1,17 @@
 import axios from 'axios';
+require('dotenv').config();
+
+
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-// const API_KEY = 'f33a484cf794d08d0148764789aaba32';
 const API_KEY = '92a368be320fc5e9a0816ae44a1a686f';
+// const API_KEY = process.env.API_KEY;
 
 export const fetchWeather = async (query) => {
-    
-    const { data } = await axios.get(URL, {
+
+    const  { data }  = await axios({
+        method: 'get',
+        url: URL,
         params: {
             q: query,
             units: 'imperial',
